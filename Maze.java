03459849d940aa6743c4be62a15cbd918600a4b4
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2023-01-23 14:58:32 -0800

    yes

--- Maze.java
+++ Maze.java
@@ -6,4 +6,26 @@ public class Maze extends JPanel{
 
     private Drawable[] items = new Drawable[3];
 
+    public void paintComponent (Graphics g) {
+
+        super.paintComponent(g);
+        g.setColor(Color.LIGHT_GRAY);
+        g.drawRect(0, 0, 500, 500);
+
+        for (int i = 0; i < 500; i+=10) {
+            g.drawLine(i, 0, i, 500);
+        }
+        for (int i = 0; i < 500; i+=10) {
+            g.drawLine(0, i, 500, i);
+        }
+        for (int i = 0; i < 500; i+=10) {
+            items[i].draw(g);
+        }
+    }
+
+    Maze(Pacman pacman, Ghost ghost){
+        items[0] = pacman;
+        items[1] = ghost;
+        items[2] = new PowerDot(300, 30);
+    }
 }
